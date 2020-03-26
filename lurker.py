6a5ad786712738688a1bee2e86dfa8cf29c1a160
Chelby Rhoades <chelbyjrhoades@gmail.com>
2020-03-25 19:06:15 -0500

    changing the lurker file so that it can process urls

--- lurker.py
+++ lurker.py
@@ -1,25 +1,64 @@
-#using beautiful soup and standard urllib
-'''note - the urllib version used isn't compatible with earlier versions
-	of urllib; so Python is required to be updated to version 3.
-'''
+'''************************
+Chelby Rhoades
+************************'''
+'''********IMPORTS***********'''
 import requests
 import time
 import operator
 #from collections import defaultdict
-from sklearn.feature_extraction.text import TfidfVectorizer #testing purposes only
-#import urllib.requests
-#import urllib.parser
-from bs4 import BeautifulSoup
+from sklearn.feature_extraction.text import TfidfVectorizer #testing purposes only - I'm only using this to see the accuracy of my results.
+from bs4 import BeautifulSoup #THIS IS ONE OF THE MOST USED ONES
 
-'''************************'''
+
 
 '''*******FUNCTIONS********'''
+#find all links on the page
+def links(url):
+    html = requests.get(url).content
+    bsObj = BeautifulSoup(html, 'lxml')
+
+    links = bsObj.findAll('a')
+    finalLinks = set()
+    for link in links:
+        finalLinks.add(link.attrs['href'])
+    return finalLinks
+
+
 def computeTF(wordDict, bow):
 	tfDict = {}
 	bowCount = len(bow)
 	for word, count in wordDict.items():
 		tfDict[word] = count/float(bowCount)
 	print(tfDict)
+
+def processLink(urlList):
+	robotWord = "robot"
+	ommited = ['-','i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 
+'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have',
+ 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here',
+ 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now']
+
+	robotFile = False
+	time.sleep(5) #pause for a bit. We want to be polite.
+	for i in urlList:
+		url = requests.get(i)#"http://s2.smu.edu/~fmoore") #should find schedule.htm
+		pagetext = url.text
+		soup = BeautifulSoup(pagetext, "html.parser")
+		txt = soup.get_text()
+		tokens = txt.split()
+
+	for w in tokens:
+		if w == robotWord:
+			robotFile = True
+			break
+		if w not in ommited:
+			words.append(w)
+			wordfreq.append(tokens.count(w)) #this is counting how many times the words appear in the document
+	
+	if robotFile == False:
+		return pairs = (dict(zip(words, wordfreq))) # putting the two lists together
+	else:
+		return pairs = (dict(zip("NULL", "NULL")))
 
 
 
@@ -28,30 +67,18 @@ def computeTF(wordDict, bow):
 words = []
 wordfreq = []
 foundURLS = []
+alreadySearchedURLS = []
 #STOP WORDS LIST INSIDE OF A LIST
-ommited = ['-','i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 
-'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have',
- 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here',
- 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now']
 
 #LET'S START!
 #our first url - the given website
-urlList = ["https://s2.smu.edu/~fmoore/schedule.htm", "https://s2.smu.edu/~fmoore"]
-for i in urlList:
-	url = requests.get(i)#"http://s2.smu.edu/~fmoore") #should find schedule.htm
-	pagetext = url.text
-	soup = BeautifulSoup(pagetext, "html.parser")
-	txt = soup.get_text()
-	tokens = txt.split()
-	for w in tokens:
-	#w = w.lower() #putting all the words in lowercase to combat case sensitivity
-		if w not in ommited:
-			words.append(w)
-			wordfreq.append(tokens.count(w)) #this is counting how many times the words appear in the document
-	time.sleep(5) #pause for a bit. We want to be polite.
-pairs = (dict(zip(words, wordfreq))) # putting the two lists together
+starterUrl = ["https://s2.smu.edu/~fmoore"]
+alreadySearchedURLS.insert(starterUrl[0])
 
-sortedPairs = dict(sorted(pairs.items(), key=operator.itemgetter(1),reverse=True))
+returnedWords = processLink(starterUrl)
+
+
+#sortedPairs = dict(sorted(pairs.items(), key=operator.itemgetter(1),reverse=True))
 #print('Dictionary in descending order by value : ',sortedPairs)
 
 
